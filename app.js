e4fe4eac9495f8a1578c59ae7f4c9ab2dad63cf1
hibafayyan <hibafayyan@gmail.com>
2026-07-07 23:15:09 +0500

    start pdf

---
+++ app.js
@@ -0,0 +1,39 @@
+// 1. Show an alert box on click on a link.
+
+let link = document.getElementById("alertClick");
+
+link.addEventListener("click", function (event) {
+    event.preventDefault(); // Link ko open hone se rokta hai
+    alert("Hello! Link clicked.");
+});
+
+//_______________________________________________________________
+// 2. Display some Mobile images in browser. On click on an
+// image Show the message in alert to user.
+
+
+
+//_______________________________________________________________
+
+// 3. Display 10 student records in table and each row should contain a delete
+// button. If you click on a button to delete a record, entire row should be
+// deleted.
+// Before delete
+// After click on delete button on “Mark ” row
+
+
+
+//_______________________________________________________________
+
+// 4. Display an image in browser. Change the picture on mouseover and set the
+// first picture on mouseout.
+
+
+
+//_______________________________________________________________
+
+// 5. Show a counter in browser. Counter should increase on click on increase
+// button and decrease on click on decrease button. And show updated counter
+// value in browser.
+
+
